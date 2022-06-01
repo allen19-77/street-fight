03b48a4504b7b4c55502f3671353e98aee9472d4
allen19-77 <allen.19@list.ru>
2022-06-01 19:39:24 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const attackButton = document.getElementById('attack-button')
+attackButton.onclick = heroAttacks
+
+function heroAttacks() {
+	
+}
+
+function enemyGetsAttack() {
+
+}
+
+function changeCurrentFighter() {
+	
+}
